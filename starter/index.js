ba93c26e0f4c34e2a0d82d428c8e793b0b102eb2
Darsing <catalunasiegfried@gmail.com>
2023-01-23 23:37:13 +0000

    added local storage and data is persists when page reloads

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,5 @@
 // to show the current date
-var todaysDate = moment("12PM","hA");
+var todaysDate = moment();
 var todaysTime = todaysDate.format("hA");
 $("#currentDay").text(todaysDate.format("dddd, MMMM Do"));
 
@@ -31,13 +31,14 @@ function getWorkHours() {
 
         //create a column for the time
         var timeEl = $('<div>');
-        timeEl.attr('class', 'col-lg-1 col-md-1 hour');
+        timeEl.attr('class', 'col-lg-2 col-md-2 hour');
         timeEl.text(workHours[i]);
         var currentTimeIndex = workHours.indexOf(todaysTime)
         //create a column for text area, where user inputs on calendar
         var textInput = $('<textarea>');
-        textInput.attr('class', 'col-lg-10 col-md-10');
-        //checks for current time
+        textInput.attr('class', 'col-lg-9 col-md-9');
+        textInput.attr('id',workHours[i]);
+        //checks for current time and appliess class if row is present, past or future
         if (currentTimeIndex === workHours.indexOf(workHours[i])) {
             textInput.addClass('present');
         } else if (currentTimeIndex > workHours.indexOf(workHours[i])) {
@@ -49,6 +50,7 @@ function getWorkHours() {
         //create a save button
         var saveButton = $('<button>');
         saveButton.attr('class', 'fa fa-save col-lg-1 col-md-1 saveBtn');
+        saveButton.attr('id', workHours[i]);
 
         //then append the button, textarea and timeEl to row
         rowDiv.append(timeEl);
@@ -58,27 +60,42 @@ function getWorkHours() {
     }
 }
 
-var taskInput = $('textarea')
-var storedTask= localStorage.getItem('savedTasks');
+var storedTask;
 var task;
-
-function saveTask(event){
-    event.preventDefault();
-    var task = $('textarea').val();
-    localStorage.setItem("savedTasks", task);
-    displayTasks();
-}
-
-function displayTasks(){
-    taskInput.text(storedTask)
-}
-
-displayTasks();
-
-console.log(todaysTime)
 
 //runs when the page loads up
 getWorkHours();
 
 //this event listener runs when user wants to save a task
-$('button').on('click',saveTask)
+$('button').on('click',function(event){
+    event.preventDefault();
+    var task = event.target.previousElementSibling.value;
+    localStorage.setItem(event.target.attributes[1].value, task);
+});
+
+// this function runs when the page loads, and persists the data
+// from local storage
+
+function displayTasks() {
+    var nineAm = localStorage.getItem("9AM");
+    var tenAm = localStorage.getItem("10AM");
+    var elevenAm = localStorage.getItem("11AM");
+    var twelvePm = localStorage.getItem("12PM");
+    var onePm = localStorage.getItem("1PM");
+    var twoPm = localStorage.getItem("2PM");
+    var threePm = localStorage.getItem("3PM");
+    var fourPm = localStorage.getItem("4PM");
+    var fivePm = localStorage.getItem("5PM");
+
+    $("#9AM").text(nineAm);
+    $("10AM").text(tenAm);
+    $("#11AM").text(elevenAm);
+    $("#12PM").text(twelvePm);
+    $("#1PM").text(onePm);
+    $("#2PM").text(twoPm);
+    $("#3PM").text(threePm);
+    $("#4PM").text(fourPm);
+    $("#5PM").text(fivePm);
+};
+
+displayTasks();
